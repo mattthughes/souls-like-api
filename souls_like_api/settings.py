dc81300d95700d0f497bc9de2f707d3d231a7140
Matthew Hughes <matt.h17@yahoo.com>
2024-07-02 17:57:35 +0000

    added profiles model to api

--- souls_like_api/settings.py
+++ souls_like_api/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
+    'profiles'
 ]
 
 MIDDLEWARE = [
