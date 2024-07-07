a6ac4328a1378666e82c5a16ec7f34ebff9c0520
Matthew Hughes <matt.h17@yahoo.com>
2024-07-07 19:54:40 +0000

    Added files field to profile model

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -15,6 +15,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'bio', 'image', 'is_owner',
+            'content', 'bio', 'image', 'files', 'is_owner',
             'posts_count',
         ]
